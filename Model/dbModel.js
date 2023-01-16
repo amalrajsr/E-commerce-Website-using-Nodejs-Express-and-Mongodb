58b6bf656926cc781e7dba2bfedb89a29642f092
amalrajsr <sramalraj123@gmail.com>
2023-01-16 21:50:35 +0530

    add comment

--- Model/dbModel.js
+++ Model/dbModel.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-//Creating database collection
+//Creating database collection for mongodb
 mongoose.connect("mongodb://127.0.0.1:27017/project").then(()=>{
 
 console.log("Database running successfully")
