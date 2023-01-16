f3ce193296672feb48ce9f971ed550e47ec77709
amalrajsr <sramalraj123@gmail.com>
2023-01-17 00:39:03 +0530

    changes made

--- Middleware/file_upload.js
+++ Middleware/file_upload.js
@@ -2,7 +2,7 @@ const multer =require('multer')
 
 let storage=multer.diskStorage({
     destination: function(req,file,cb){
-        cb(null,'./public/admin/assets/category')
+        cb(null,'/admin/assets/category')
     },
     filename:function(req,file,cb){
         const name=file.filename+'-'+file.originalname;
